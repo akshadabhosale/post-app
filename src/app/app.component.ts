664a52d7f9e88ed50a2586aca017ba8a400dc122
Akshada <akshadabhosale122@gmail.com>
2024-08-12 17:03:27 +0530

    Click event handling and refercing the input to get the value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,12 @@ import { Component } from "@angular/core";
   selector:"app-root",
   templateUrl:"./app.component.html"
 })
-export class AppComponent{}
+export class AppComponent{
+
+  addPost=(content:HTMLTextAreaElement)=>{
+    // to get value of this html content dir is user
+    console.dir(content)
+    console.log(content.value)
+    console.log("Click Event Fired ")
+  }
+}
