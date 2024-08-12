76c2cfa284a27b55b901042c4f92e48adf6b4605
Akshada <akshadabhosale122@gmail.com>
2024-08-12 16:55:55 +0530

    Post App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers:[],
+    declarations:[AppComponent],
+    imports:[BrowserModule],
+    bootstrap:[AppComponent]
+
 })
-export class AppModule { }
+export class AppModule{}
